c602cd8be57f3796873a9964847ef4a7e5245140
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-09-24 19:03:01 -0500

    Some changes in the main class, a singleton was added in addition to some changes with the "service"

--- src/main/java/me/cxmilo/chat/client/ChatController.java
+++ src/main/java/me/cxmilo/chat/client/ChatController.java
@@ -1,11 +1,14 @@
 package me.cxmilo.chat.client;
 
+import me.cxmilo.chat.client.chat.thread.ShutdownThread;
 import me.cxmilo.chat.client.service.Service;
 import me.cxmilo.chat.client.service.impl.ClientConnectService;
 
 import java.util.Scanner;
 
 public class ChatController {
+
+    private static Service conn;
 
     public static void main(String[] args) {
         // The scanner is temporally, im just testing rn
@@ -15,7 +18,24 @@ public class ChatController {
         String name = scanner.nextLine();
         String channel = scanner.nextLine();
 
-        Service conn = new ClientConnectService(name, channel);
+        conn = new ClientConnectService(name, channel);
+
+        if (!conn.start()) {
+            System.out.println("An error occurred, please check your connection or server connection and try again!");
+            return;
+        }
+
         conn.start();
+
+        // Shutdown hook
+        // I don't think this will work, sorry it's my first time trying this
+        Runtime.getRuntime().addShutdownHook(new ShutdownThread());
+    }
+
+    /**
+     * @return User connection to the server
+     */
+    public static Service getConnection() {
+        return conn;
     }
 }
